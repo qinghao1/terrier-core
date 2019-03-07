49bec1f7cfbb27f33a13a8d12590aae41969790a
limyongsong <honestkiddy@gmail.com>
2019-03-07 21:10:25 +0100

    bugfix: fix rank number issue

--- var/results/filter_passages.py
+++ var/results/filter_passages.py
@@ -13,16 +13,22 @@ with open('../../code/qrel/qrel_test.txt') as csv_file:
 			docid = col[2].split('-')[0]
 			queries[col[0]] = docid
 		
-		
+
 for file in os.listdir(file_loc):
 	if file.endswith(".res") and not file.endswith("_f.res"):
 		new_col = []
 		with open(file) as csv_file:
 			reader = csv.reader(csv_file, delimiter=' ')
+			rank = 0
 			for col in reader:
 				if col[0] in queries.keys():
+					if new_col != []:
+						if col[0] != new_col[-1][0]:
+							rank = 0
 					if col[2].split('-')[0] == queries[col[0]]:
+						col[3] = str(rank)
 						new_col.append(col)
+						rank += 1
 						
 		new_name = file.split('.res')[0] + "_f.res"
 		csv2 = open(new_name, "w") 
